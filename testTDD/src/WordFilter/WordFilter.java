375ddade7b978ff81f49a008d7492efddc7da9ee
Tatsuya Miyamoto <hondasports@gmail.com>
2014-09-02 16:34:18 +0900

    Added method for clear word list and reRegist word list.

--- testTDD/src/WordFilter/WordFilter.java
+++ testTDD/src/WordFilter/WordFilter.java
@@ -1,22 +1,42 @@
 package WordFilter;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class WordFilter {
 
-	String word;
+	ArrayList<String> wordList;
 
-	public String getWord() {
-		return word;
-	}
-
-	public WordFilter(String string) {
-		word = string;
+	public WordFilter() {
+		// TODO Auto-generated constructor stub
 	}
 
 	public boolean detect(String string) {
-		return string.contains(word);
+		for (String word : wordList) {
+			if (string.contains(word) == true) return true; 
+		}
+		return false;
 	}
 
 	public String censor(String string) {
-		return string.replace(word, "<censored>");
+		for (String word : wordList) {
+			string = string.replace(word, "<censored>");
+		}
+		return string; 
+	}
+
+	public int registNGWord(String string) {
+		wordList = new ArrayList<String>(Arrays.asList(string.split(",")));
+		return wordList.size();
+	}
+
+	public int clearNGword() {
+		wordList.clear();
+		return wordList.size();
+	}
+
+	public ArrayList<String> getWordList() {
+		// TODO Auto-generated method stub
+		return wordList;
 	}
 }
--- testTDD/src/WordFilter/WordFilterTest.java
+++ testTDD/src/WordFilter/WordFilterTest.java
@@ -8,43 +8,60 @@ import org.junit.Test;
 /**
  * @author miyamoto
  * 仕様
- * 1. 特定文字列の検出する
- * 2. 特定文字列を置換する
+ * 1. NGワードの検出する
+ * 2. NGワードを置換する
+ * 3. NGワードを複数登録できるようにする
  * Content URL is #20 of http://www.slideshare.net/t_wada/tddbc-exercise
  */
 public class WordFilterTest {
 
+	WordFilter wordFilter;
 	@Before
 	public void setUp() throws Exception {
-	}
-
-	@Test
-	public void testInitializeWithParam(){
-		String testString = "aaa";
-		WordFilter wordFilter = new WordFilter(testString);
-
-		assertNotNull(wordFilter);
-		assertEquals(testString,wordFilter.getWord());
+		wordFilter = new WordFilter();
 	}
 	
 	@Test
 	public void testDetectSimpleWord(){
-		WordFilter wordFilter = new WordFilter("aaa");
+		wordFilter.registNGWord("aaa");
 		assertTrue(wordFilter.detect("aaa bbb"));
 		assertFalse(wordFilter.detect("aabbbaa"));
 	}
-	
+
 	@Test
 	public void testDetectWordInSentence(){
-		WordFilter wordFilter = new WordFilter("晩御飯");
+		wordFilter.registNGWord("晩御飯");
 		assertTrue(wordFilter.detect("今日の晩御飯はハンバーグ"));
 		assertFalse(wordFilter.detect("今日の昼ごはんはハンバーグ"));
 	}
 
 	@Test
 	public void testCensor(){
-		WordFilter wordFilter = new WordFilter("ハンバーグ");
+		wordFilter.registNGWord("ハンバーグ");
 		assertEquals("今日の晩御飯は<censored>", wordFilter.censor("今日の晩御飯はハンバーグ"));
 		assertNotEquals("今日の晩御飯は<censored>", wordFilter.censor("今日の晩御飯はパンバーグ"));
+	}
+	
+	@Test
+	public void testRegistMultiNGWordAndCensor(){
+		WordFilter wordFilter = new WordFilter();
+		assertEquals(2, wordFilter.registNGWord("晩御飯,ハンバーグ"));
+		assertEquals("今日の<censored>は<censored>", wordFilter.censor("今日の晩御飯はハンバーグ"));
+	}
+
+	@Test
+	public void testRegistSingleNGWord(){
+		WordFilter wordFilter = new WordFilter();
+		assertEquals(1, wordFilter.registNGWord("晩御飯"));
+	}
+	
+	@Test
+	public void testCleanAndReRegistNGWord(){
+		wordFilter.registNGWord("晩御飯,ハンバーグ");
+		assertEquals(0, wordFilter.clearNGword());
+
+		String[] wordList ={"お昼", "から揚げ定食"}; 
+		wordFilter.registNGWord("お昼,から揚げ定食");
+		assertArrayEquals(wordList, wordFilter.getWordList().toArray());
 	}
 }
